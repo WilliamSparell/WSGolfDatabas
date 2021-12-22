4c30272e5b8f06f3eef06467c6ebdbe851c20c33
William Spårell <williamatlasnova@gmail.com>
2021-12-22 17:00:45 +0100

    public

--- WSGolfDatabas/utiles/CRUDHelper.cs
+++ WSGolfDatabas/utiles/CRUDHelper.cs
@@ -22,80 +22,80 @@ namespace WSGolfDatabas.utiles
                 );
                 if (existingWeather == null) existingWeather = new WeatherTable { Weather = weather };
 
-                 = new ScoreTable { Date = date, Score = score,  = weather,  = course };
+                 = new ScoreTable { Date = date, Score = score, Weather = existingWeather, Course = existingCourse };
                 db.Scores.Add();
                 db.SaveChanges();
 
                 return;
             }
         }
-        public static ScoreTable FindAndDeleteRound(DateTime date)
-        {
-            using (var db = new GolfContext())
-            {
-                var round = db.Scores.
-                FirstOrDefault(
+        //public static ScoreTable FindAndDeleteRound(DateTime date)
+        //{
+        //    using (var db = new GolfContext())
+        //    {
+        //        var round = db.Scores.
+        //        FirstOrDefault(
 
-                p => p.Date == date
+        //        p => p.Date == date
 
-                );
-                if (round != null) // om rundan finns, radera den
-                {
-                    db.Scores.Remove(round);
-                    db.SaveChanges();
-                    Console.WriteLine("rundan är nu Borta!");
-                }
-                else
-                    Console.WriteLine("Hittade ingen runda med det datumet");
+        //        );
+        //        if (round != null) // om rundan finns, radera den
+        //        {
+        //            db.Scores.Remove(round);
+        //            db.SaveChanges();
+        //            Console.WriteLine("rundan är nu Borta!");
+        //        }
+        //        else
+        //            Console.WriteLine("Hittade ingen runda med det datumet");
 
-                return round;
-            }
-        }
-        public static ScoreTable FindAndUpdateRound(DateTime date)
-        {
-            using (var db = new GolfContext())
-            {
-                var round = db.Scores.
-                FirstOrDefault(
+        //        return round;
+        //    }
+        //}
+        //public static ScoreTable FindAndUpdateRound(DateTime date)
+        //{
+        //    using (var db = new GolfContext())
+        //    {
+        //        var round = db.Scores.
+        //        FirstOrDefault(
 
-                    p => p.Date == date
+        //            p => p.Date == date
 
-                );
-                if (round != null) // om personen finns, ta bort den
-                {
-                    Update(round);
-                    db.Scores.Update(round);
-                    db.SaveChanges();
-                    Console.WriteLine("Personen är nu uppdaterad");
-                }
-                return round;
-            }
-        }
+        //        );
+        //        if (round != null) // om personen finns, ta bort den
+        //        {
+        //            Update(round);
+        //            db.Scores.Update(round);
+        //            db.SaveChanges();
+        //            Console.WriteLine("Personen är nu uppdaterad");
+        //        }
+        //        return round;
+        //    }
+        //}
 
-        private static void Update(ScoreTable score)
-        {
-            Console.WriteLine("Vad vill du uppdatera?");
-            Console.WriteLine("1. Score");
-            Console.WriteLine("2. Väder");
-            Console.WriteLine("3. Bana");
-            string input = Console.ReadLine();
+        //private static void Update(ScoreTable score)
+        //{
+        //    Console.WriteLine("Vad vill du uppdatera?");
+        //    Console.WriteLine("1. Score");
+        //    Console.WriteLine("2. Väder");
+        //    Console.WriteLine("3. Bana");
+        //    string input = Console.ReadLine();
 
-            if (input == "1")
-            {
-                int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
-                score.Score = updated;
-            }
-            if (input == "2")
-            {
-                int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
-                score.WeatherId = updated;
-            }
-            if (input == "3")
-            {
-                int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
-                score.CourseId = updated;
-            }
+        //    if (input == "1")
+        //    {
+        //        int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
+        //        score.Score = updated;
+        //    }
+        //    if (input == "2")
+        //    {
+        //        int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
+        //        score.WeatherId = updated;
+        //    }
+        //    if (input == "3")
+        //    {
+        //        int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
+        //        score.CourseId = updated;
+        //    }
 
-        }
+        //}
     }
 }
