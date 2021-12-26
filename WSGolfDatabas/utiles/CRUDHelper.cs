f2ecd0a4a85f14cda568aacd1586f3f3bac5bcee
William Spårell <williamatlasnova@gmail.com>
2021-12-26 21:27:33 +0100

    nästan klar

--- WSGolfDatabas/utiles/CRUDHelper.cs
+++ WSGolfDatabas/utiles/CRUDHelper.cs
@@ -22,12 +22,20 @@ namespace WSGolfDatabas.utiles
                 );
                 if (existingWeather == null) existingWeather = new WeatherTable { Weather = weather };
 
-                var round = new ScoreTable { Date = date, Score = score, WeatherId = existingWeather, CourseId = existingCourse };
-                db.Scores.Add(round);
-                db.SaveChanges();
+                var dateCheck = db.Scores.
+                FirstOrDefault(
+                    p => p.Date == date
+                );
+
+                if (dateCheck == null)
+                {
+                    var round = new ScoreTable { Date = date, Score = score, WeatherId = existingWeather, CourseId = existingCourse };
+                    db.Scores.Add(round);
+                    db.SaveChanges();
+                }
             }
         }
-        public static ScoreTable FindAndDeleteRound()
+        public static ScoreTable FindAndDeleteRound(DateTime date)
         {
             using (var db = new GolfContext())
             {
@@ -46,51 +54,31 @@ namespace WSGolfDatabas.utiles
                 return round;
             }
         }
-        //public static ScoreTable FindAndUpdateRound(DateTime date)
-        //{
-        //    using (var db = new GolfContext())
-        //    {
-        //        var round = db.Scores.
-        //        FirstOrDefault(
+        public static ScoreTable FindAndUpdateRound(DateTime date)
+        {
+            using (var db = new GolfContext())
+            {
+                var round = db.Scores.
+                FirstOrDefault(
 
-        //            p => p.Date == date
+                    p => p.Date == date
 
-        //        );
-        //        if (round != null) // om personen finns, ta bort den
-        //        {
-        //            Update(round);
-        //            db.Scores.Update(round);
-        //            db.SaveChanges();
-        //            Console.WriteLine("Personen är nu uppdaterad");
-        //        }
-        //        return round;
-        //    }
-        //}
+                );
+                if (round != null)
+                {
+                    Update(round);
+                    db.Scores.Update(round);
+                    db.SaveChanges();
+                    Console.WriteLine("Personen är nu uppdaterad");
+                }
+                return round;
+            }
+        }
 
-        //private static void Update(ScoreTable score)
-        //{
-        //    Console.WriteLine("Vad vill du uppdatera?");
-        //    Console.WriteLine("1. Score");
-        //    Console.WriteLine("2. Väder");
-        //    Console.WriteLine("3. Bana");
-        //    string input = Console.ReadLine();
-
-        //    if (input == "1")
-        //    {
-        //        int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
-        //        score.Score = updated;
-        //    }
-        //    if (input == "2")
-        //    {
-        //        string updated = WriteAndInput("Skriv in nya score: ");
-        //        score.WeatherId = updated;
-        //    }
-        //    if (input == "3")
-        //    {
-        //        int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
-        //        score.CourseId = updated;
-        //    }
-
-        //}
+        private static void Update(ScoreTable score)
+        {
+            int.TryParse(WriteAndInput("Skriv in nya score: "), out int updated);
+            score.Score = updated;
+        }
     }
 }
